e5be14b74b19b92ed74868dcd918e0d90e53b635
Kevin Grigorenko <kevin@myplaceonline.com>
2016-03-15 22:16:41 -0700

    Disable SELinux in bootstrap

--- cookbooks/bootstrap_server/recipes/default.rb
+++ cookbooks/bootstrap_server/recipes/default.rb
@@ -1,28 +1,17 @@
-myplaceonline_logo = %q{
-                        _                            _ _            
-                       | |                          | (_)           
-  _ __ ___  _   _ _ __ | | __ _  ___ ___  ___  _ __ | |_ _ __   ___ 
- | '_ ` _ \| | | | '_ \| |/ _` |/ __/ _ \/ _ \| '_ \| | | '_ \ / _ \
- | | | | | | |_| | |_) | | (_| | (_|  __/ (_) | | | | | | | | |  __/
- |_| |_| |_|\__, | .__/|_|\__,_|\___\___|\___/|_| |_|_|_|_| |_|\___|
-             __/ | |                                                
-            |___/|_|                                                
-} + "\n\n"
+# There is no point to call Chef::Log because it won't go to the knife
+# bootstrap output (see parent README for details)
 
 directory "#{data_bag_item("server", "server")["secrets_dir"]}" do
   mode "0700"
 end
 
-Chef::Log.info %{#{myplaceonline_logo}
-  Updating all packages and rebooting...
-}
-
 # Could've used shutdown with +1 but it was broken at time of writing
 # (https://github.com/systemd/systemd/issues/1120)
 execute "commands" do
   command %{
+    setenforce Permissive && \
     dnf -y install at && \
-    dnf -y update && \
+    dnf -y --setopt=deltarpm=false update && \
     echo "reboot" | at now + 1 minutes && \
     echo "Please wait 2 minutes while the server reboots..."
   }
