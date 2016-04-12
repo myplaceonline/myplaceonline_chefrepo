6e6a45884ebe57b22d79d4847db80bb3f502dd98
Kevin Grigorenko <kevin@myplaceonline.com>
2016-04-11 22:20:03 -0700

    Fix bug inserting duplicate lines in /etc/fstab

--- cookbooks/server_finish/recipes/default.rb
+++ cookbooks/server_finish/recipes/default.rb
@@ -7,7 +7,7 @@ end
 ruby_block "nfs client" do
   block do
     file = Chef::Util::FileEdit.new("/etc/fstab")
-    file.insert_line_if_no_match("/#{node.nfs.client.host}/", "#{node.nfs.client.host}:#{node.nfs.server.directory} #{node.nfs.client.mount} nfs timeo=5,intr")
+    file.insert_line_if_no_match(/#{Regexp.escape(node.nfs.client.host)}/, "#{node.nfs.client.host}:#{node.nfs.server.directory} #{node.nfs.client.mount} nfs timeo=5,intr")
     file.write_file
   end
 end
