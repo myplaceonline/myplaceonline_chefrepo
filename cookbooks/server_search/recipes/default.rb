202e0ff3fca67bfad0f9e3b7c650b774fd2f1360
Kevin Grigorenko <kevin@myplaceonline.com>
2016-05-11 21:34:23 -0700

    Add logstash

--- cookbooks/server_search/recipes/default.rb
+++ cookbooks/server_search/recipes/default.rb
@@ -1,4 +1,15 @@
-package %w{elastic-curator elasticdump elasticsearch}
+# https://www.elastic.co/guide/en/logstash/current/installing-logstash.html
+file "/etc/yum.repos.d/influxdb.repo" do
+  content %q{[logstash-2.3]
+name=Logstash repository for 2.3.x packages
+baseurl=http://packages.elastic.co/logstash/2.3/centos
+gpgcheck=0
+gpgkey=http://packages.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+} 
+end
+
+package %w{elastic-curator elasticdump elasticsearch logstash}
 
 template "/etc/elasticsearch/elasticsearch.yml" do
   source "elasticsearch.yml.erb"
