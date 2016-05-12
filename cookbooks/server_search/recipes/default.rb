baae26695e050206d07780d6bb0515a67d6deb84
Kevin Grigorenko <kevin@myplaceonline.com>
2016-05-11 21:21:39 -0700

    Add ElasticSearch configuration file

--- cookbooks/server_search/recipes/default.rb
+++ cookbooks/server_search/recipes/default.rb
@@ -1,1 +1,10 @@
 package %w{elastic-curator elasticdump elasticsearch}
+
+template "/etc/elasticsearch/elasticsearch.yml" do
+  source "elasticsearch.yml.erb"
+  notifies :restart, "service[elasticsearch]", :immediately
+end
+
+service "elasticsearch" do
+  action [:enable, :start]
+end
